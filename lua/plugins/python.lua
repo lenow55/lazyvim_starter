78acc69544eb376506a81c875fb888983b0101b7
lenow55 <99322377+lenow55@users.noreply.github.com>
2025-02-25 22:21:53 +0300

    configure python server

---
+++ lua/plugins/python.lua
@@ -0,0 +1,74 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        ---@class lspconfig.Config
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              analysis = {
+                diagnosticMode = "workspace",
+                autoImportCompletions = true,
+                exclude = { ".venv/*", "venv/*" },
+                ---ignore = { "/home/lenow/.cache/pypoetry/virtualenvs/*", "/home/lenow/miniconda3/envs/*" },
+                diagnosticSeverityOverrides = {
+                  reportAny = "warning",
+                  reportUnknownMemberType = "warning",
+                  reportUnknownArgumentType = "warning",
+                  reportUnknownParameterType = "warning",
+                  reportMissingParameterType = "warning",
+                  reportUnknownVariableType = "warning",
+                  reportDeprecated = "information",
+                },
+              },
+              -- pyright = {
+              --   disableOrganizeImports = false,
+              -- },
+            },
+          },
+          cmd = { "basedpyright-langserver", "--stdio" },
+          cmd_env = { LC_ALL = "ru" },
+          on_attach = function(client, _)
+            client.server_capabilities.executeCommandProvider = {
+              commands = {
+                "basedpyright.createtypestub",
+                "basedpyright.organizeimports",
+                "basedpyright.unusedImport",
+                "basedpyright.import",
+                "basedpyright.writeBaseline",
+              },
+              workDoneProgress = true,
+            }
+          end,
+        },
+      },
+    },
+  },
+  {
+    "stefanboca/venv-selector.nvim",
+    branch = "sb/push-rlpxsqmllxtz",
+    enabled = true,
+    cmd = "VenvSelect",
+    opts = {
+      settings = {
+        options = {
+          notify_user_on_venv_activation = true,
+        },
+      },
+    },
+    --  Call config for python files and load the cached venv automatically
+    ft = "python",
+    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
+  },
+  -- {
+  --   "linux-cultist/venv-selector",
+  --   opts = {
+  --     settings = {
+  --       hooks = {},
+  --     },
+  --   },
+  -- },
+}
