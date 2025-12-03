fa1dc21cf3960e4770de1eaf7d06aa29922ff85b
lenow55 <99322377+lenow55@users.noreply.github.com>
2025-12-03 23:51:23 +0300

    Update mcphub.lua

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -8,14 +8,33 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    build = "bundled_build.lua", -- Bundles `mcp-hub` binary along with the neovim plugin для винды не работает
+    build = "bundled_build.lua", -- Bundles `mcp-hub` binary along with the neovim plugin
     opts = {
       log = {
         -- level = vim.log.levels.TRACE,
-        to_file = true,
-        file_path = "/home/lenow/.local/state/nvim/mcphub.log",
+        -- to_file = true,
+        -- file_path = "/home/lenow/.local/state/nvim/mcphub.log",
       },
       use_bundled_binary = true, -- Use local `mcp-hub` binary
+    },
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            show_result_in_chat = true, -- Show mcp tool results in chat
+            make_vars = true, -- Convert resources to #variables
+            make_slash_commands = true, -- Add prompts as /slash commands
+          },
+        },
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
     },
   },
 }
